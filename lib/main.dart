ab4e4d9c5ae7e4325ce135f2551f24678041f371
Flutter-Libya <131480121+Flutter-Libya@users.noreply.github.com>
2023-08-06 12:32:09 +0200

    commit one
    one commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './models/database_provider.dart';
+// screens
+import './screens/category_screen.dart';
+import './screens/expense_screen.dart';
+import './screens/all_expenses.dart';
+
+void main() {
+  runApp(ChangeNotifierProvider(
+    
+    create: (_) => DatabaseProvider(),
+    child: const MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      initialRoute: CategoryScreen.name,
+      routes: {
+        CategoryScreen.name: (_) => const CategoryScreen(),
+        ExpenseScreen.name: (_) => const ExpenseScreen(),
+        AllExpenses.name: (_) => const AllExpenses(),
+      },
+    );
+  }
+}
---
+++ lib/widgets/expense_screen/expense_card.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../../constants/icons.dart';
+import '../../models/expense.dart';
+import './confirm_box.dart';
+
+class ExpenseCard extends StatelessWidget {
+  final Expense exp;
+  const ExpenseCard(this.exp, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Dismissible(
+      key: ValueKey(exp.id),
+      confirmDismiss: (_) async {
+        showDialog(
+          context: context,
+          builder: (_) => ConfirmBox(exp: exp),
+        );
+        return null;
+      },
+      child: ListTile(
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Icon(icons[exp.category]),
+        ),
+        title: Text(exp.title),
+        subtitle: Text(DateFormat('MMMM dd, yyyy').format(exp.date)),
+        trailing: Text(NumberFormat.currency(locale: 'ar_sa', symbol: 'LD')
+            .format(exp.amount)),
+      ),
+    );
+  }
+}
